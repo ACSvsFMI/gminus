c4f0bbd3a47967d1b7d6cbbf62f030902c59f1c7
Theodor Vararu <theo@tvararu.ro>
2012-12-08 11:35:50 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gminus::Application.config.secret_token = '3a1070e61bac1ee300c44be7c286d7fdc881757955252198799e2c1d37740cc8726ad4eaa290802db28c163e25a7149c529613200d2fd524bea80068682008a0'

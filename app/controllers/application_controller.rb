e7088ed40a18b12a813e8058a16cd7095bc1267d
Theodor Vararu <theo@tvararu.ro>
2012-12-08 22:23:27 +0200

    Add different layout for signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # another way
+  layout :another_by_method
+  private
+  def another_by_method
+    if current_user.nil?
+      "logged_off"
+    else
+      "application"
+    end
+  end
 end

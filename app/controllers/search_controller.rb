5f5ea3f5ba65182b6a5dd6925a22e9d5facbf665
Theodor Vararu <theo@tvararu.ro>
2012-12-08 17:06:51 +0200

    Get get request working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,7 @@ class SearchController < ApplicationController
   end
 
   def show
-    @result = params[:search]
+    get = RestClient.get "https://www.googleapis.com/plus/v1/people?query=#{ params[:search].parameterize }&maxResults=10&key=#{ API_KEY }"
+    @result = ActiveSupport::JSON.decode(get)
   end
 end
